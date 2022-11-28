e2f693b04aae76b6932882e95e739d53d26bcd00
Valent9088 <andriy.valent1312@gmail.com>
2022-11-28 15:52:10 +0200

    Update all_queries.sql, adding Andriy's homework

--- all _queries.sql
+++ all _queries.sql
@@ -17,3 +17,14 @@ select count(order_id), customer_id from orders
 where customer_id in
 (select customer_id from customers where country='France') 
 group by customer_id having count(order_id)>1
+
+Andriy 14
+Show first and last names of the employees as well as the count
+of orders each of them have received during the year 1997 (use a subquery).
+
+Select employees.first_name, employees.last_name, SUM(order_details.quantity) AS total_quantity
+from orders
+left join employees on orders.employee_id = employees.employee_id
+left join order_details on orders.order_id = order_details.order_id
+WHERE date_part('year', orders.order_date) = 1997
+GROUP BY employees.first_name, employees.last_name
