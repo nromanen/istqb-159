2e3438557305098c1ae5f5434458e778c46cde2a
KaterynaGoncharova <kgoncharova14@gmail.com>
2022-11-29 21:21:44 +0200

    Add my part of homework in common file and add test design homework

--- all _queries.sql
+++ all _queries.sql
@@ -28,3 +28,14 @@ left join employees on orders.employee_id = employees.employee_id
 left join order_details on orders.order_id = order_details.order_id
 WHERE date_part('year', orders.order_date) = 1997
 GROUP BY employees.first_name, employees.last_name
+
+Kateryna Goncharova 
+Show first and last names of the employees as well as the count of their orders shipped after required date during the year 1997 (use left join).
+
+SELECT LAST_NAME, FIRST_NAME, COUNT(*)
+FROM EMPLOYEES
+LEFT JOIN ORDERS ON EMPLOYEES.EMPLOYEE_ID = ORDERS.EMPLOYEE_ID
+WHERE EXTRACT (YEAR FROM (REQUIRED_DATE)) = '1997' 
+AND EXTRACT (YEAR FROM (SHIPPED_DATE)) = '1997'
+AND REQUIRED_DATE < SHIPPED_DATE
+GROUP BY EMPLOYEES.EMPLOYEE_ID
